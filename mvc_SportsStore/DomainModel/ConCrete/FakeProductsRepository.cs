927eb3001d708b0d08c62f4aef40e199344713a5
TigerKim <df072@nate.com>
2012-07-19 23:30:38 +0900

    131-1

--- mvc_SportsStore/DomainModel/ConCrete/FakeProductsRepository.cs
+++ mvc_SportsStore/DomainModel/ConCrete/FakeProductsRepository.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DomainModel.Abstract;
+using DomainModel.Entities;
 
 namespace DomainModel.ConCrete
 {
-    public class FakeProductsRepository : IProductsRepository
+    public class FakeProductsRepository : IProductsRespository
     {
         private static IQueryable<Product> fakeProducts = new List<Product>
-                                                              {
-                                                                  new Product {Name = "Football", Price = 25},
-                                                                  new Product {Name = "Surf board", Price = 179},
-                                                                  new Product {Name = "Running shoes", Price = 95}
-                                                              }.AsQueryable();
+                                                    {
+                                                        new Product {Name = "Football", Price = 25},
+                                                        new Product {Name = "Surf board", Price = 179},
+                                                        new Product {Name = "Running shoes", Price = 95}
+                                                    }.AsQueryable();
 
         public IQueryable<Product>  Products
         {
---
+++ mvc_SportsStore/WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DomainModel.Abstract;
+
+
+namespace WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //
+        // GET: /Products/
+        private IProductRepository productRepository;
+        public ProductsController ()
+        {
+            productRepository = new FakeProductRepository();
+        }
+
+        public ActionResult List()
+        {
+            return View(productRepository.Products.ToList());
+        }
+
+        
+
+    }
+}
